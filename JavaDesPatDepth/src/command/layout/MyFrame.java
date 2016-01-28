27b8709fb25fa387c6808a84cbdd5d7481d46984
Yan Gao <gaoyan126@googlemail.com>
2016-01-28 12:51:57 +0100

    Add command pattern examples

--- JavaDesPatDepth/src/command/layout/MyFrame.java
+++ JavaDesPatDepth/src/command/layout/MyFrame.java
@@ -1,7 +1,68 @@
 package command.layout;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MyFrame extends JFrame {
 
+	JPanel content = new JPanel();
+
+	ActionListener a = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			content.removeAll();
+			content.setLayout(new FlowLayout());
+			content.add(new JButton("1"));
+			content.add(new JButton("2"));
+			content.add(new JButton("3"));
+			content.add(new JButton("4"));
+			content.add(new JButton("5"));
+			content.updateUI();
+		}
+	};
+
+	ActionListener a2 = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			content.removeAll();
+			content.setLayout(new BorderLayout());
+			content.add(new JButton("NORTH"), BorderLayout.NORTH);
+			content.add(new JButton("SOUTH"), BorderLayout.SOUTH);
+			content.add(new JButton("WEST"), BorderLayout.WEST);
+			content.add(new JButton("EAST"), BorderLayout.EAST);
+			content.add(new JButton("CENTER"), BorderLayout.CENTER);
+			content.updateUI();
+		}
+
+	};
+
+	public void init() {
+		JPanel p = new JPanel();
+		JButton btn = new JButton("FlowLayout Show");
+		JButton btn2 = new JButton("BorderLayout Show");
+		p.add(btn);
+		p.add(btn2);
+
+		add(p, BorderLayout.NORTH);
+		add(content);
+
+		btn.addActionListener(a);
+		btn2.addActionListener(a2);
+
+		setSize(400, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new MyFrame().init();
+	}
 }
--- JavaDesPatDepth/src/command/layout/MyFrame2.java
+++ JavaDesPatDepth/src/command/layout/MyFrame2.java
@@ -1,16 +1,81 @@
 package command.layout;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MyFrame2 extends JFrame implements ActionListener {
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	JPanel content = new JPanel();
 
+	class FlowBtn extends JButton implements IReceiver {
+
+		public FlowBtn(String name) {
+			super(name);
+		}
+
+		@Override
+		public void response() {
+			content.removeAll();
+			content.setLayout(new FlowLayout());
+			content.add(new JButton("1"));
+			content.add(new JButton("2"));
+			content.add(new JButton("3"));
+			content.add(new JButton("4"));
+			content.add(new JButton("5"));
+			content.updateUI();
+		}
 	}
 
+	class BorderBtn extends JButton implements IReceiver {
+
+		public BorderBtn(String name) {
+			super(name);
+		}
+
+		@Override
+		public void response() {
+			content.removeAll();
+			content.setLayout(new BorderLayout());
+			content.add(new JButton("NORTH"), BorderLayout.NORTH);
+			content.add(new JButton("SOUTH"), BorderLayout.SOUTH);
+			content.add(new JButton("WEST"), BorderLayout.WEST);
+			content.add(new JButton("EAST"), BorderLayout.EAST);
+			content.add(new JButton("CENTER"), BorderLayout.CENTER);
+			content.updateUI();
+		}
+	}
+
+	public void init() {
+		JPanel p = new JPanel();
+		FlowBtn btn = new FlowBtn("FlowLayout Show");
+		BorderBtn btn2 = new BorderBtn("BorderLayout Show");
+		p.add(btn);
+		p.add(btn2);
+
+		add(p, BorderLayout.NORTH);
+		add(content);
+
+		btn.addActionListener(this);
+		btn2.addActionListener(this);
+
+		setSize(400, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		IReceiver obj = (IReceiver) e.getSource();
+		obj.response();
+	}
+
+	public static void main(String[] args) {
+		new MyFrame2().init();
+	}
 }
